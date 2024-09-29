5ed212a72fe209b8375520a7e4a1e5183d254d26
Alice Li <nameal1234@gmail.com>
2024-09-29 10:23:35 -0400

    get sean baml

--- baml_ws/main.py
+++ baml_ws/main.py
@@ -1,5 +1,5 @@
 from baml_client.sync_client import b
-from baml_client.types import Functions
+from baml_client.types import Functions, OpCodes
 
 
 def extractFuncs(raw: str) -> Functions:
@@ -9,26 +9,26 @@ def extractFuncs(raw: str) -> Functions:
     return response.dict()
 
 
-def OpFunctions(raw: str, other: str) -> str:
+def OpFunctions(raw: str) -> OpCodes:
     # BAML's internal parser guarantees ExtractResume
     # to be always return a Resume type
-    response = b.OptFuncs(raw, other)
-    return response
+    response = b.OptimizationSummary(raw)
+    return response.dict()
 
 
-def main():
-    tmp = str()
-    with open("./camelCase.c") as t:
-        tmp = t.read()
-    funcs = extractFuncs(tmp)
-    for i in funcs["all_functions"]:
-        print(i["func_body_wo_brackets"])
-        t = b.OptimizationSummary(i["func_body_wo_brackets"]).dict()
-        print("\n Summary:")
-        print(t["optimization_summary"])
-        print("\n code:")
-        print(t["newly_optimized_code"])
-        print("\n\n\n\n\n\n")
+# def main():
+    # tmp = str()
+    # with open("./camelCase.c") as t:
+    #     tmp = t.read()
+    # funcs = extractFuncs(tmp)
+    # for i in funcs["all_functions"]:
+    #     print(i["func_body_wo_brackets"])
+    #     t = b.OptimizationSummary(i["func_body_wo_brackets"]).dict()
+    #     print("\n Summary:")
+    #     print(t["optimization_summary"])
+    #     print("\n code:")
+    #     print(t["newly_optimized_code"])
+    #     print("\n\n\n\n\n\n")
 
 
 if __name__ == "__main__":
